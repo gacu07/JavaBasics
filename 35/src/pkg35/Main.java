ffac878da274e4bbbbcffe49152cdedbcd0b7139
Janek Gacki <gacu07git@gmail.com>
2018-09-24 21:45:44 +0200

    New tasks

--- 35/src/pkg35/Main.java
+++ 35/src/pkg35/Main.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package pkg34;
+package pkg35;
 import java.util.*;
 /**
  *
@@ -15,22 +15,18 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
         // TODO code application logic here
-    ArrayList<String> Colors=new ArrayList<>();
-    Colors.add("Blue");
+        ArrayList<String> Colors=new ArrayList<>();
+        Colors.add("Blue");
         Colors.add("Pink");
-            Colors.add("GREY");
-                Colors.add("Green");
-                
-                System.out.println(Colors);
-                
-                
-                Colors.remove(2);
-                
-                
-                  System.out.println(Colors);
-                
+        Colors.add("GREY");
+        Colors.add("Green");
+        
+        if(Colors.contains("Pink")){
+        System.out.println("Found the element");
+                }
+     
+      
     }
     
 }
